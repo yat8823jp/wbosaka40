e0d42936fc892fc6ade94fbc899c825c74b15af0
hiromi_iwai <jpwbdsgn@gmai.com>
2015-04-04 15:38:32 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>WordBench Osaka #40</title>
+	<link rel="stylesheet" href="<?php bloginfo ('stylesheet_url'); ?>" type="text/css" />
+</head>
+<body>
+	<div class="sticky-post">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat consectetur mollitia, magni rerum doloribus iste iusto, nostrum quidem sunt quibusdam assumenda reiciendis. Corporis tempore debitis iusto quaerat illo, dolorem sed.
+	</div>
+	<!-- /.sticky-post -->
+</body>
+</html>
